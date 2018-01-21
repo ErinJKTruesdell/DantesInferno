3b9a300e3fc6bc6cb5526d35bb5f772a155666db
Latitude3450\Erin <nern.strudel@gmail.com>
2018-01-20 22:22:57 -0500

    did some t hings

---
+++ DantesInferno/Assets/Scripts/Conversation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conversation : MonoBehaviour {
+
+    //public UIManager uiman
+    //public Character parentChar
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void setRel(int rel)
+    {
+
+        //parentChar.setRel(rel);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ DantesInferno/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    //public UIManager uiman;
+    
+    //public List<String> charNames;
+    //public Dictionary<Character, int> allCharRels;
+
+	// Use this for initialization
+	void Start () {
+        //charNames=
+    }
+
+    public void loadCommand()
+    {
+
+        //allCharRels = new Dictionary<Character, int>();
+        //foreach(String s in charNames)
+        //{
+        //Character c = new Character(s);
+        //
+        //c.rel = playerprefs.GetInt(s);
+        //}
+    }
+
+    public void saveCommand()
+    {
+        //foreach(KeyValuePair<Character, int> kvp in allCharRels)
+        {
+            //PlayerPrefs.SetInt(kvp.Key.Name, kvp.value);
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
